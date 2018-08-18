fb0380f44c5233dbf2c539ae5dc2f91f9ea9a57d
Irla Svendsen <irlasvendsen@gmail.com>
2018-08-17 18:35:12 -0600

    adding revision from udacity Added aria label for itens Added sw.js for restaurant.html

--- sw.js
+++ sw.js
@@ -19,29 +19,29 @@ self.addEventListener('install', function(event) {
     });
 });
 
-self.addEventListener('fetch', function(event) {
-event.waitUntil(
+self.addEventListener('active', function(event){
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        var request = event.request.clone();
-        return fetch(request).then( function(response){
-          if(!response || response !==200){
+});
+
+self.addEventListener('fetch', function(event) {
+  event.waitUntil(
+      event.respondWith(
+        caches.match(event.request).then(function(response) {
+          if (response) {
             return response;
           }
-
-          var responseCache = response.clone();
-          caches.open(CACHE).then(function(cache){
-            cache.put(event.request,responseCache);
-          });
-          return response;
-        });
-      }
+          var request = event.request.clone();
+            return fetch(request).then(function(response){
+              if(!response || response !==200){
+                return response;
+              }
+                var responseCache = response.clone();
+                caches.open(CACHE).then(function(cache){
+                  cache.put(event.request,responseCache);
+                });
+                return response;
+            });
+        })
     )
   );
-);
 });
