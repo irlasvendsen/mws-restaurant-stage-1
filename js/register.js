ddfea1e86a47b9b6b527025c5a4e72efb2d95f8b
Irla Svendsen <irlasvendsen@gmail.com>
2018-08-31 16:04:23 -0600

    deleted all the content of / that was moved to app/

--- js/register.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  self.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('ServiceWorker registered! ', registration.scope);
-    }, function(error) {
-      console.log('ServiceWorker failed to register!: ', error);
-    });
-  });
-}
