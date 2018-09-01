b93500d2b4e91b3c0a7b2d2d829e32ee624825c2
Irla Svendsen <irlasvendsen@gmail.com>
2018-09-01 10:59:34 -0600

    Add your key on maps

--- app/js/restaurant_info.js
+++ app/js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiaXJsYXN2ZW5kc2VuIiwiYSI6ImNqazRzcnh0MDA0ODUzcm4xcjNobGQ0NnoifQ.7gdxkgIy7AGuPiZufxaKOg',
+        mapboxToken: 'Your Key',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
